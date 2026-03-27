302c4cbfe75725c54346a84e23b831b9e12625c6
Sandyravi <sandyravi104@gmail.com>
2026-03-28 02:37:11 +0530

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
+
+/* ✅ ADD HERE */
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("Mongo Error:", err);
+  });
+
 app.get("/create-admin", async (req, res) => {
   const user = new User({
     email: "admin@mnr.com",
