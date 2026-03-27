2d2911a9985b7e39eb94ba1998f03cdbd139ec6c
Sandyravi <sandyravi104@gmail.com>
2026-03-27 13:00:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,4 +22,8 @@ app.put("/api/leads/:id", (req, res) => {
   res.json({ msg: "Updated" });
 });
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
